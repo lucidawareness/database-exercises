7609c1081d2edd866ca2821ba40ca1ad3d04c7ad
Irvin Ruiz <ruizirvin084@gmail.com>
2022-04-01 11:58:18 -0500

    completed sub queries exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -2,4 +2,18 @@
 # 69 Rows
 SELECT CONCAT(first_name, ' ', last_name)
 FROM employees
-WHERE hire_date = (SELECT emp_no )
+WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no = '101010');
+
+# TODO: Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+SELECT COUNT(title)
+FROM titles
+WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
+# TODO: Find all the current department managers that are female
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE to_date > CURDATE())
+  AND emp_no IN (SELECT emp_no FROM employees WHERE gender = 'f');
+
+# TODO: Find all the department names that currently have female managers
